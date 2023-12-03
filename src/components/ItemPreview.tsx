9c310c88439a38002e48deddace8cb5dfb770651
ndrlpzm <andrea.lm923@gmail.com>
2023-12-03 17:19:08 +0100

    implements item selection using useState

--- src/components/ItemPreview.tsx
+++ src/components/ItemPreview.tsx
@@ -1,10 +1,23 @@
 import Item from "../data/objects/Item";
 interface ItemPreviewInput {
   item: Item;
+  isSelected: boolean;
+  setSelectedItem: React.Dispatch<React.SetStateAction<number | undefined>>;
 }
-export default function ItemPreview({ item }: ItemPreviewInput) {
+export default function ItemPreview({
+  item,
+  isSelected,
+  setSelectedItem,
+}: ItemPreviewInput) {
+  function handleClick(event: React.MouseEvent<HTMLElement, MouseEvent>): void {
+    if (!isSelected) setSelectedItem(item.index);
+  }
+
   return (
-    <article>
+    <article
+      onClick={handleClick}
+      className={isSelected ? "selected-item" : ""}
+    >
       <img alt={item.description} src={item.image}></img>
     </article>
   );
--- src/pages/ItemList.tsx
+++ src/pages/ItemList.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import ItemPreview from "../components/ItemPreview";
 import { itemListMock } from "../data/objects/Item";
 import "./css/ItemList.css";
 
 export default function ItemList() {
+  const [selectedItem, setSelectedItem] = useState<number | undefined>();
   return (
     <div className="item-container">
       {itemListMock.map((item) => (
-        <ItemPreview key={item.index} item={item} />
+        <ItemPreview
+          key={item.index}
+          item={item}
+          isSelected={selectedItem === item.index}
+          setSelectedItem={setSelectedItem}
+        />
       ))}
     </div>
   );
