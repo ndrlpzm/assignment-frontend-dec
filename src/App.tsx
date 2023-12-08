88e1df15beb7bc147b848be0a17702bc7cb4c6b6
ndrlpzm <andrea.lm923@gmail.com>
2023-12-08 20:26:43 +0100

    changes store provider placement

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import "./App.css";
 import { RouteProvider } from "./Routes";
 import ToastContainer from "./components/toast/ToastContainer";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <main>
-        <RouteProvider></RouteProvider>
-        <ToastContainer></ToastContainer>
-      </main>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header"></header>
+        <main>
+          <RouteProvider></RouteProvider>
+          <ToastContainer></ToastContainer>
+        </main>
+      </div>
+    </Provider>
   );
 }
 
