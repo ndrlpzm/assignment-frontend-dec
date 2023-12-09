3f52049769e02b30380c8847e27607acc8fb092f
ndrlpzm <andrea.lm923@gmail.com>
2023-12-09 13:01:17 +0100

    fixes store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import "./App.css";
 import { RouteProvider } from "./Routes";
 import ToastContainer from "./components/toast/ToastContainer";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <header className="App-header"></header>
-        <main>
-          <RouteProvider></RouteProvider>
-          <ToastContainer></ToastContainer>
-        </main>
-      </div>
-    </Provider>
+    <div className="App">
+      <header className="App-header"></header>
+      <main>
+        <RouteProvider></RouteProvider>
+        <ToastContainer></ToastContainer>
+      </main>
+    </div>
   );
 }
 
