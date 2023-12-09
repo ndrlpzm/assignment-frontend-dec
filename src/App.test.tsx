254abd1145879ed5fe263a11a37ecb04595560ac
ndrlpzm <andrea.lm923@gmail.com>
2023-12-09 12:13:54 +0100

    adds selected item test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,4 @@
-import React from "react";
-import { HttpResponse, http } from "msw";
-import { setupServer } from "msw/node";
-import {
-  render,
-  screen,
-  waitFor,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import mockData from "./data/MockData.json";
 
@@ -18,4 +10,16 @@ test("home page has items when it's done loading", async () => {
     );
   });
   expect(screen.getAllByTestId("item-preview")).not.toHaveLength(0);
+});
+
+test("home page items stay selected after click", async () => {
+  render(<App />);
+  await waitFor(() => {
+    expect(screen.getAllByTestId("item-preview")).not.toHaveLength(0);
+  });
+  fireEvent.click(screen.getAllByTestId("item-preview")[0]);
+  expect(await screen.findByText("Go Home")).toBeInTheDocument();
+
+  fireEvent.click(screen.getByText("Go Home"));
+  expect(screen.getAllByTestId("item-preview")[0]).toHaveClass("selected-item");
 });
